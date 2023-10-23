92a36cd90fe3178f4f30886883aeec6a495fc5b1
Tyler <tyler.dins88@gmail.com>
2023-10-22 19:08:47 -0700

    image and sizing

--- next-ecommfrmk-store/src/app/page.js
+++ next-ecommfrmk-store/src/app/page.js
@@ -1,59 +1,62 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import products from "products.json";
+import Warlock from "../../public/images/the-web-warlock.png"
 
 export default function Home() {
-	return (
-		<main className={styles.main}>
-			<div className={styles.description}>
-				<p>
-					Get started by editing&nbsp;
-					<code className={styles.code}>src/app/page.js</code>
-				</p>
-				<div>
-					<a
-						href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						By{" "}
-						<Image
-							src="/vercel.svg"
-							alt="Vercel Logo"
-							className={styles.vercelLogo}
-							width={100}
-							height={24}
-							priority
-						/>
-					</a>
-				</div>
-			</div>
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p>
+          Get started by editing&nbsp;
+          <code className={styles.code}>src/app/page.js</code>
+        </p>
+        <div>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{" "}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className={styles.vercelLogo}
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+        </div>
+      </div>
 
-			<div className={styles.center}>
-				<Image
-					className={styles.logo}
-					src="/next.svg"
-					alt="Next.js Logo"
-					width={180}
-					height={37}
-					priority
-				/>
-			</div>
+      <div>
+        <Image
+          src={Warlock}
+          alt="Web Warlock"
+          width={440}
+          height={440}
+          style={{
+            objectFit: "cover",
+            borderRadius: "100px",
+          }}
+        />
+      </div>
 
-			<div className={styles.grid}>
-				{products.map((product) => {
-					return (
-						<div key={product.id} className={styles.card}>
-							<h3>{product.title}</h3>
-							<p>{product.description}</p>
-							<p>${product.price}</p>
-							<p>
-								<button>Add to Cart</button>
-							</p>
-						</div>
-					);
-				})}
-			</div>
-		</main>
-	);
+      <div className={styles.grid}>
+        {products.map((product) => {
+          return (
+            <div key={product.id} className={styles.card}>
+              <h3>{product.title}</h3>
+              <p>{product.description}</p>
+              <p>${product.price}</p>
+              <p>
+                <button>Add to Cart</button>
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    </main>
+  );
 }
